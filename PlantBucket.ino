c237040f7e7eb99ed2d19df799684c151147e230
Ryan White <ry.white@gmail.com>
2018-06-18 00:43:35 +0100

    Tidied up output and added motor control code

--- PlantBucket.ino
+++ PlantBucket.ino
@@ -10,6 +10,9 @@
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+
 
 #define DHTPIN            7         // Pin which is connected to the DHT sensor.
 #define WATERPIN          A0
@@ -23,6 +26,9 @@
 //   https://learn.adafruit.com/dht/overview
 
 DHT_Unified dht(DHTPIN, DHTTYPE);
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+Adafruit_DCMotor *myMotor = AFMS.getMotor(1);
+
 
 int waterValue = 0;        // value read from the pot
 
@@ -30,13 +36,20 @@ int waterValue = 0;        // value read from the pot
 uint32_t delayMS;
 
 void setup() {
-  Serial.begin(9600); 
+  Serial.begin(9600);
   // Initialize device.
   dht.begin();
+  AFMS.begin();  // create with the default frequency 1.6KHz
+
+
   Serial.println("DHTxx Unified Sensor Example");
   // Print temperature sensor details.
   sensor_t sensor;
   dht.temperature().getSensor(&sensor);
+  while (!Serial)
+  {
+    // do nothing
+  } ;
   Serial.println("------------------------------------");
   Serial.println("Temperature");
   Serial.print  ("Sensor:       "); Serial.println(sensor.name);
@@ -44,7 +57,7 @@ void setup() {
   Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
   Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" *C");
   Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" *C");
-  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" *C");  
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" *C");
   Serial.println("------------------------------------");
   // Print humidity sensor details.
   dht.humidity().getSensor(&sensor);
@@ -55,41 +68,71 @@ void setup() {
   Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
   Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println("%");
   Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println("%");
-  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println("%");  
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println("%");
   Serial.println("------------------------------------");
   // Set delay between sensor readings based on sensor details.
   delayMS = sensor.min_delay / 1000;
 }
 
 void loop() {
-  // Delay between measurements.
-  delay(delayMS);
-  waterValue = analogRead(WATERPIN);
-  
 
+  uint8_t i;
+
+  waterValue = analogRead(WATERPIN);
   // Get temperature event and print its value.
-  sensors_event_t event;  
+  sensors_event_t event;
   dht.temperature().getEvent(&event);
-  Serial.print("Moisture Raw Value: ");
-  Serial.print(waterValue);
-  Serial.println(" Counts");
+
+  while (Serial.available() > 0) {
+    int inval = Serial.read();
+    
+    if (inval == 114) {
+      //  Serial.print("Moisture Raw Value: ");
+      Serial.print(waterValue);
+      Serial.print(",");
+
+      if (isnan(event.temperature)) {
+        Serial.print("0");
+      }
+      else {
+        //   Serial.print("Temperature: ");
+        Serial.print(event.temperature);
+        Serial.print(",");
+      }
+      // Get humidity event and print its value.
+      dht.humidity().getEvent(&event);
+
+
+      if (isnan(event.relative_humidity)) {
+        Serial.println("0");
+      }
+      else {
+        //   Serial.println("Humidity: ");
+        Serial.println(event.relative_humidity);
+        //  Serial.println("%");
+      }
+    }
+
+    if(inval == 109) {
+
+      Serial.println("Running");
+
   
-  if (isnan(event.temperature)) {
-    Serial.println("Error reading temperature!");
+       myMotor->run(BACKWARD);
+  for (i=0; i<255; i++) {
+    myMotor->setSpeed(i);  
+    delay(2);
   }
-  else {
-    Serial.print("Temperature: ");
-    Serial.print(event.temperature);
-    Serial.println(" *C");
+  delay(5000);
+  for (i=255; i!=0; i--) {
+    myMotor->setSpeed(i);  
+    delay(2);
   }
-  // Get humidity event and print its value.
-  dht.humidity().getEvent(&event);
-  if (isnan(event.relative_humidity)) {
-    Serial.println("Error reading humidity!");
+      
+    }
   }
-  else {
-    Serial.print("Humidity: ");
-    Serial.print(event.relative_humidity);
-    Serial.println("%");
-  }
+
+
+
+
 }
